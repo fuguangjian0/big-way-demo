994e94d2347fe80dd1d808ca35a50a890d3e1081
付广建 <fuguangjian0@gmail.com>
2023-08-10 23:31:48 +0800

    基本架子 - PageContainer

--- src/views/article/ArticleChannel.vue
+++ src/views/article/ArticleChannel.vue
@@ -1,11 +1,12 @@
-<script setup>
-
-</script>
-
 <template>
+  <page-container title="文章分类">
+    <template #extra>
+      <el-button type="primary"> 添加分类 </el-button>
+    </template>
 
+    主体部分
+  </page-container>
 </template>
-
-<style scoped>
-
-</style>
+<script setup>
+import PageContainer from "@/components/PageContainer.vue";
+</script>
