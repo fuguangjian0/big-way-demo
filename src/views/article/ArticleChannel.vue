a64c3aec22892c5f0f4d25d3585ef8ba1b2c0c1c
付广建 <fuguangjian0@gmail.com>
2023-08-11 08:40:45 +0800

    表格和loading效果

--- src/views/article/ArticleChannel.vue
+++ src/views/article/ArticleChannel.vue
@@ -3,8 +3,32 @@
     <template #extra>
       <el-button type="primary"> 添加分类 </el-button>
     </template>
-
-    主体部分
+    <el-table v-loading="loading" :data="channelList" style="width: 100%">
+      <el-table-column label="序号" width="100" type="index"> </el-table-column>
+      <el-table-column label="分类名称" prop="cate_name"></el-table-column>
+      <el-table-column label="分类别名" prop="cate_alias"></el-table-column>
+      <el-table-column label="操作" width="100">
+        <template #default="{ row }">
+          <el-button
+              :icon="Edit"
+              circle
+              plain
+              type="primary"
+              @click="onEditChannel(row)"
+          ></el-button>
+          <el-button
+              :icon="Delete"
+              circle
+              plain
+              type="danger"
+              @click="onDelChannel(row)"
+          ></el-button>
+        </template>
+      </el-table-column>
+      <template #empty>
+        <el-empty description="没有数据" />
+      </template>
+    </el-table>
   </page-container>
 </template>
 <script setup>
@@ -12,7 +36,17 @@ import PageContainer from "@/components/PageContainer.vue";
 import {artGetChannelsService} from "@/api/article";
 const channelList = ref([])
 const getChannelList = async () => {
+  loading.value = true
   const res = await artGetChannelsService()
   channelList.value = res.data.data
+  loading.value = false
 }
+const onEditChannel = (row) => {
+  console.log(row);
+}
+const onDelChannel = (row) => {
+  console.log(row);
+}
+const loading = ref(false)
+
 </script>
